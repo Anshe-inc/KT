610b601c047ef0d9a63c1fcb09e0adb16dd86654
Anshe-inc <shcherbakov.andrei@phystech.edu>
2022-02-22 23:19:24 +0300

    tests added respective to operations

--- 1/main.cpp
+++ 1/main.cpp
@@ -2,5 +2,19 @@
 
 int main(){
     Fraction frac(2, -4);
-    std::cout << frac;
+    std::cout << frac << std::endl;
+
+    Fraction a(1, 2);
+    std::cout << a << std::endl;
+    Fraction b(2, 4);
+    std::cout << a << '+' << b << '=' <<a + b << std::endl;    
+    
+    std::cout << a << '+' << 2 << '=' << a + 2 << std::endl;    
+    std::cout << 2 << '+' << a << '=' << 2 + a << std::endl;     
+    
+    Fraction c(1, 4);
+    std::cout << a << '-' << c << '=' << a - c << std::endl;    
+    
+    std::cout << a << '-' << 2 << '=' << a - 2 << std::endl;    
+    std::cout << 2 << '-' << a << '=' << 2 - a << std::endl; 
 }
